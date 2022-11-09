7c9a5d79b7f5b77cd6b96f174ab02387bf9cafe9
tran-trong-dinh <trantrongdinh0221@gmail.com>
2022-11-09 23:40:09 +0700

    Popular

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,18 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor:true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 
